67f3abad5ae13487849ac40928f92758682f0027
İsmail Codar <icodar@hotmail.com>
2025-06-15 10:35:27 +0300

    daprWorkflow da tek bir generator fonksiyon kullanılıyor

--- apps/flowera/backend/src/workflow/dapr/runner.ts
+++ apps/flowera/backend/src/workflow/dapr/runner.ts
@@ -7,19 +7,55 @@ import type { IDaprWorkflowRunnerContext } from "./types";
 export const createDaprWorkflowFromGraph = (graph: IWorkflowGraph) => {
   const activityByName = new Map<string, TWorkflowActivity<any, any>>();
 
-  const daprWorkflow: TWorkflow = async function* (ctx: WorkflowContext, payload: any): any {
+  const daprWorkflow: TWorkflow = async function* (daprContext: WorkflowContext, payload: any): any {
     debugger;
     const workflowResult = new Map<string, any>();
     const graphRoots = findRootGraphNodes(graph);
     for (const rootNode of graphRoots) {
-      const result = yield runGraphNode({
-        graph,
-        dapr: ctx,
-        graphNode: rootNode,
-        activityByName,
-        payload,
-      });
-      workflowResult.set(rootNode.name, result);
+      const graphNodeResult = new Map<string, any>();
+      const stack: IDaprWorkflowRunnerContext[] = [
+        {
+          graphNode: rootNode,
+          payload,
+        },
+      ];
+
+      while (stack.length > 0) {
+        const currentStackItem = stack.pop();
+        if (!currentStackItem) continue;
+
+        // Eğer zaten bu node işlendi ise atla
+        if (graphNodeResult.has(currentStackItem.graphNode.name)) continue;
+
+        const activity = activityByName.get(currentStackItem.graphNode.name);
+        if (!activity) continue;
+
+        // const sourceActivityResult = runActivity(ctx.dapr, activity, ctx.graphNode, ctx.payload);
+        // TODO callActivity yerine waitForExternalEvent whenAll vs olabilecek
+        /* TODO activity işlemleri:
+            - direk çağrılabilir(ctx.callActivity)
+            - node tipi if/switch ise koşullu çağrılabilir
+            - node tipi wait ise beklemeye geçilebilir,
+            - node tipi return ise sonuç return edilebilir.
+        */
+        const sourceActivityResult = yield daprContext.callActivity(activity, {});
+
+        graphNodeResult.set(currentStackItem.graphNode.name, sourceActivityResult);
+
+        const outputEdges = graph.edges.filter((edge) => edge.source === currentStackItem.graphNode.name);
+        for (const edge of outputEdges) {
+          const nextNode = graph.nodes.find((node) => node.name === edge.target);
+          if (nextNode) {
+            stack.push({
+              ...currentStackItem,
+              graphNode: nextNode,
+              payload: sourceActivityResult,
+            });
+          }
+        }
+      }
+
+      workflowResult.set(rootNode.name, graphNodeResult);
     }
     return { sonuc: true };
   };
@@ -51,53 +87,12 @@ export const registerWorkflowToDapr = (
   }
 };
 
-const runGraphNode = function* (ctx: IDaprWorkflowRunnerContext) {
-  debugger;
-  const graphNodeResult = new Map<string, any>();
-
-  const activity = ctx.activityByName.get(ctx.graphNode.name);
-  if (activity) {
-    const sourceActivityResult = yield runActivity(ctx.dapr, activity, ctx.graphNode, ctx.payload);
-
-    const outputEdges = ctx.graph.edges.filter((edge) => edge.source === ctx.graphNode.name);
-    for (const edge of outputEdges) {
-      const graphNode = ctx.graph.nodes.find((node) => node.name === edge.target);
-      if (graphNode) {
-        const result = yield runGraphNode({ ...ctx, graphNode, payload: sourceActivityResult });
-        graphNodeResult.set(graphNode.name, result);
-      }
-    }
-  }
-  return graphNodeResult;
-};
-
 const worflowNodeToDaprActivity = (node: IWorkflowNode): TWorkflowActivity<any, any> => {
   // TODO node.baseType dan başlayarak node tipine göre işlem yaptırılacak. mail gönder, http request, http response, http webhook, wait?, if?, switch?
   return async (_: WorkflowActivityContext, request: any) => {
     const result = true;
     return result;
   };
-};
-
-const runActivity = function* (
-  daprContext: WorkflowContext,
-  activity: TWorkflowActivity<any, any>,
-  node: IWorkflowNode,
-  payload: any,
-) {
-  // TODO callActivity yerine waitForExternalEvent whenAll vs olabilecek
-  /* TODO activity işlemleri:
-            - direk çağrılabilir(ctx.callActivity)
-            - node tipi if/switch ise koşullu çağrılabilir
-            - node tipi wait ise beklemeye geçilebilir,
-            - node tipi return ise sonuç return edilebilir.
-        */
-  if (node.baseType === "condition") {
-    if (node.conditionType === "if") {
-    }
-  }
-  const taskResult = yield daprContext.callActivity(activity, payload);
-  return taskResult;
 };
 
 /** MAIL
--- apps/flowera/backend/src/workflow/dapr/types.ts
+++ apps/flowera/backend/src/workflow/dapr/types.ts
@@ -3,9 +3,6 @@ import type { IWorkflowGraph, IWorkflowNode } from "../nodes/_all";
 import type { TWorkflowActivity } from "@dapr/dapr/types/workflow/Activity.type";
 
 export interface IDaprWorkflowRunnerContext {
-  graph: IWorkflowGraph;
-  dapr: WorkflowContext;
   graphNode: IWorkflowNode;
-  activityByName: Map<string, TWorkflowActivity<any, any>>;
   payload: any;
 }
